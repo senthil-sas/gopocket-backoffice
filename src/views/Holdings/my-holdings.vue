8b5a5748a691f03823673746d470a872bcd12ec2
akashcodifi <akashraja@codifi.in>
2024-05-29 14:26:40 +0530

    added

--- src/views/Holdings/my-holdings.vue
+++ src/views/Holdings/my-holdings.vue
@@ -10,7 +10,7 @@
                     
         <tbody>
             <tr v-for="(item, id) in getHoldingsData" :key="id" class="border-b">
-                <td class="pl-3 py-4 text-sm primary-color dark:text-[#94A3B8] relative">{{ item.symbol }} {{ getHoldingsData && getHoldingsData.length > 0 }}</td>
+                <td class="pl-3 py-4 text-sm primary-color dark:text-[#94A3B8] relative"> {{ item.symbol }} </td>
                 <td class="pr-3 py-4 text-sm primary-color dark:text-[#94A3B8] relative text-right">{{ item.qty }}</td>
                 <td class="pr-3 py-4 text-sm primary-color dark:text-[#94A3B8] relative text-right">{{ item.buy_avg }}</td>
                 <td class="pr-3 py-4 text-sm primary-color dark:text-[#94A3B8] relative text-right">{{ item.buy_value }}</td>
