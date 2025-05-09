f12f5f50e775fb4793d64240f843ea36fc3dc65d
SivaselvanRaman <sivaselvan@codifi.in>
2025-05-09 12:38:56 +0530

    bse and bfo added in profile - segment section

--- src/views/profile/segments.vue
+++ src/views/profile/segments.vue
@@ -4,14 +4,15 @@
             <div class="rounded-lg p-3 border-[0.02rem] dark:border-[#232325]">
                 <label class="my-2 primary-color !font-semibold text-sm" id="accinfo_label_eq">Equities</label>
                 <div class="crd-body mr-3">
-                <div class="my-3 whitespace-nowrap text-xs" :class="{ 'opacity-50' : getProfileData.segment.nse == 'INACTIVE' }">
+                <div class="my-3 whitespace-nowrap text-xs">
                     <button id="accinfo_label_nse_eq" class="bluebtn mr-2" >NSE EQ</button>
+                    <button id="accinfo_label_nse_eq" class="bluebtn mr-2" >BSE EQ</button>
                     <button id="accinfo_label_nse_mf" class="bluebtn" >NSE MF</button>
                 </div>
                 <p class="secondary-color dark:text-[#94A3B8] mb-4 text-sm min-h-[60px]">Equity derivatives, as well as contract-based assets,such as bonds, commodities and currencies, on NSE.</p>
-                    <button v-if="getProfileData.segment.nse?.toUpperCase() == 'ACTIVE'" id="accinfo_eq_isactive" class="commonbtn green">Active</button>   
-                    <button v-if="getProfileData.segment.nse?.toUpperCase() == 'INACTIVE'" id="accinfo_eq_isactive" class="commonbtn">Activate Segment</button>  
-                    <button v-if="getProfileData.segment.nse?.toUpperCase() == 'DORMANT'" id="accinfo_eq_isactive" class="commonbtn">Start-KYC</button>                 
+                    <button v-if="getProfileData.segment.nse?.toUpperCase() == 'ACTIVE' && getProfileData.segment.bse?.toUpperCase() == 'ACTIVE'" id="accinfo_eq_isactive" class="commonbtn green">Active</button>   
+                    <button v-if="getProfileData.segment.nse?.toUpperCase() == 'INACTIVE' || getProfileData.segment.bse?.toUpperCase() == 'INACTIVE'" id="accinfo_eq_isactive" class="commonbtn">Activate Segment</button>  
+                    <button v-if="getProfileData.segment.nse?.toUpperCase() == 'DORMANT' || getProfileData.segment.bse?.toUpperCase() == 'DORMANT'" id="accinfo_eq_isactive" class="commonbtn">Start-KYC</button>                 
             </div>
             </div>
             <div class="rounded-lg p-3 border-[0.02rem] dark:border-[#232325]">
@@ -19,13 +20,14 @@
                 <div class="crd-body mr-3">
                 <div class="my-3 whitespace-nowrap text-xs">
                     <button id="accinfo_label_nfo" class="graybtn mr-2">NFO</button>
+                    <button id="accinfo_label_nfo" class="graybtn mr-2">BFO</button>
                     <button id="accinfo_label_nse_cur" class="graybtn">NSE CUR</button>
                 </div>
                 <p class="secondary-color dark:text-[#94A3B8] mb-4 text-sm min-h-[60px]">Equity derivatives, as well as contract-based assets, such as bonds, commodities & currencies, on NSE</p>
                 <!-- <button id="accinfo_derivatives_isactive" class="commonbtn" >Enable Segment</button> -->
-                    <button v-if="getProfileData.segment.nfo?.toUpperCase() == 'ACTIVE'" id="accinfo_eq_isactive" class="commonbtn green">Active</button>   
-                    <button v-if="getProfileData.segment.nfo?.toUpperCase() == 'INACTIVE'" id="accinfo_eq_isactive" class="commonbtn">Activate Segment</button>  
-                    <button v-if="getProfileData.segment.nfo?.toUpperCase() == 'DORMANT'" id="accinfo_eq_isactive" class="commonbtn">Start-KYC</button>
+                    <button v-if="getProfileData.segment.nfo?.toUpperCase() == 'ACTIVE' && getProfileData.segment.bfo?.toUpperCase() == 'ACTIVE'" id="accinfo_eq_isactive" class="commonbtn green">Active</button>   
+                    <button v-if="getProfileData.segment.nfo?.toUpperCase() == 'INACTIVE' || getProfileData.segment.bfo?.toUpperCase() == 'INACTIVE'" id="accinfo_eq_isactive" class="commonbtn">Activate Segment</button>  
+                    <button v-if="getProfileData.segment.nfo?.toUpperCase() == 'DORMANT' || getProfileData.segment.bfo?.toUpperCase() == 'DORMANT'" id="accinfo_eq_isactive" class="commonbtn">Start-KYC</button>
                 </div>
             </div>
             <div class="rounded-lg p-3 border-[0.02rem] dark:border-[#232325]">
